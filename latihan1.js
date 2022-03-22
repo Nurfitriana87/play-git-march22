d4f85c0b4d49139d9d3a105e6c37df92716fbb72
nurfitriana87 <nurfitrianafina08@gmail.com>
2022-03-22 14:34:51 +0800

    initial commit

---
+++ latihan1.js
@@ -0,0 +1,72 @@
+// // console.log("hello world");
+
+// let firstName = "Nur";
+// let lastName = "Fitriana";
+// let fullName = firstName + " " + lastName; // spasi dihitung juga sebeagai karakternya
+
+// console.log(fullName.length);
+
+// //data integer =
+// //
+
+// let message = "hello world from alterra";
+// console.log(typeof message);
+
+// message = 12345;
+
+// console.log(typeof message);
+
+// let infinity = +infinity; //suatu angka yang tak terhingga
+
+// console.log(typeof infinity);
+
+// let a = 100;
+// let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// let isTrue = true;
+// let isFalse = false;
+// let isGreater = 5 > 1;
+
+// console.log(5 > 2); //bisa langsung tapi kyknya bad practice
+
+// let a;
+// let b = null; //mau dikosongkan
+
+// console.log(a, b);
+
+// let a, b, add, sub, mul, div, mod;
+// a = 100;
+// b = 5;
+// add = a + b;
+// sub = a - b;
+// mul = a * b;
+// div = a / b;
+// mod = a % b;
+
+// console.log(add, sub, mul, div, mod);
+
+// console.log("increement (++)");
+
+// a++;
+// console.log(a);
+// b--;
+// console.log(b);
+
+let c = 5;
+let d = 5;
+
+// console.log(c === d);
+
+let g = 6;
+let h = 7;
+let i = 2;
+let j = 2;
+let k = 10;
+
+//
